9e5f367b8cc2055f8a8efa7e6cdb0a6daeb341e7
Dave Kennedy <dave-kennedy@outlook.com>
2016-03-01 22:23:32 -0700

    Added update_ddns_ca_dir config setting

--- update-ddns.sh
+++ update-ddns.sh
@@ -23,11 +23,17 @@ while IFS="= " read key value; do
             ddns_pass="$value";;
         update_ddns_ip_src)
             ip_src="$value";;
+        update_ddns_ca_dir)
+            ca_dir="$value";;
     esac
 done < "$config"
 
 if [ -z "$ip_src" ]; then
     ip_src="https://myip.dnsomatic.com"
+fi
+
+if [ -z "$ca_dir" ]; then
+    ca_dir="/etc/ssl/certs"
 fi
 
 if [ -z "$ddns_user" -o -z "$ddns_pass" ]; then
@@ -66,7 +72,7 @@ mv "$local_ip" "$cache_dir"
 if [ "$update" = true ]; then
     echo "Sending update to $ddns_service..." | tee -a "$log"
 
-    if ! curl -sSu "$ddns_user:$ddns_pass" "$ddns_service"; then
+    if ! curl --capath "$ca_dir" -sSu "$ddns_user:$ddns_pass" "$ddns_service"; then
         echo "Error: could not send update to $ddns_service" | tee -a "$log"
         exit 1
     fi
